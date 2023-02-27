09e7bb9cb86e6c5d12eacb952b5820e2b97640bd
naveendoddi <prasadnaveen847@gmail.com>
2023-02-27 12:27:54 +0530

    emp

--- src/userdashboard.js
+++ src/userdashboard.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Navbar from "./navbar";
-
+import { data } from "./dataEmployee";
 function Yes(){
 
+
+    for(let i=0; i<100; i++){
+        console.log(data[i])
+    }
     return(
         <>
             <Navbar />
+
+            
         </>
     )
 }
 export default Yes
-//comitmig
