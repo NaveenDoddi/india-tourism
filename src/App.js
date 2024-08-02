00957fb8615eb24b8009610f33c6c11cf4751b1d
naveendoddi <prasadnaveen847@gmail.com>
2024-08-02 13:54:52 +0530

    prgressive bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 // import Carasoul from "./carousel";
-import Form from "./formValidation";
-import TODO from "./todo1";
+import Form from "./components/formValidation";
+import TODO from "./components/todo1";
+import Dropdown from "./components/dropdown";
+import Search from "./components/search";
+import Progressbar from "./components/progressbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App(){
     return(
     <>
-        < Form />
+        < Progressbar />
     </>
     )
 }
